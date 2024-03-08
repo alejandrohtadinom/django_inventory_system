31952773bdcb085d24e11bfce3fd047624fee379
Alejandro H Tadino M <tadinomalejandro@gmail.com>
2024-03-08 16:49:05 -0400

    [FIX] Typo in product model
    The product model file has a typo in the ManyToManyFiled

--- product/models.py
+++ product/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    category = ManyToManyField("Category")
+    category = models.ManyToManyField("Category")
 
 
 class Category(models.Model):
