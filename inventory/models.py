833fbd92e5f67b035207695d03a9315549e098bb
Alejandro H Tadino M <tadinomalejandro@gmail.com>
2024-03-08 16:55:53 -0400

    [ADD] Warehouse model
    The code that defined the model for the warehouse was moved to its own file

--- inventory/models.py
+++ inventory/models.py
@@ -7,14 +7,3 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     descrition = models.TextField()
     qtty = models.IntegerField()
-
-
-class Warehouse(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-
-
-class Location(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
