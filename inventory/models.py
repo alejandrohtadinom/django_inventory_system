9610b917956133bfa242d0fb382819e222500e94
Alejandro H Tadino M <tadinomalejandro@gmail.com>
2024-03-18 19:15:50 -0400

    [ADD] Supplier model
    - Added supplier model (all fields are text fields)
    - Added supplier field to inventory model

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from product.models import Product
+from supplier.models import Supplier
 from warehouse.models import Warehouse
 
 
 # Main item model
 class Inventory(models.Model):
-    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product)
     value = models.DecimalField(max_digits=12, decimal_places=2)
-    location = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier)
+    location = models.ForeignKey(Warehouse)
     qtty = models.DecimalField(max_digits=12, decimal_places=2)
--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    notes = models.TextField()
